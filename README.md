3caf3d34f0d2254b3a43e2fcea55be9d33b7753a
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-02-05 11:08:38 -0500

    gettingCloser

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-<body>
-
-<div style="color: #F8DDF4">
-        <br>
-        <h2>Live Journal But Anonymous</h2>
+<body style="color: #F8DDF4">
+    <div>
+        <h2>Live Journal But Private</h2>
         <!-- <img src='./images/me.jpg' alt='Liz' height="100" width="100"/> -->
         <img src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/anatomical-heart_1fac0.png' alt='heart' height="100" width="100" />
-</div>
+    </div>
 <link rel="stylesheet" href="path/to/module/css/colors.css">
-
 </body>
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const container = document.querySelector('.main-container')
-    const blogInput = document.querySelector('#blogs')
-    const demoInput = document.querySelector('#video')
-    const edInput = document.querySelector('#ed')
-    const workInput = document.querySelector('#work')
-    const buttons = document.querySelector('#buttons')
+const entryDiv = document.querySelector('.entriesDiv')
+const button = document.querySelector('button')
+
+const getInputOrSubmitNewPost = e => {
+    if (e.target.innerText === "New Post"){
+        provideImputForm()
+    } else {
+        displayPost()
+    }
+}
+
+const displayPost = () => {
+    const input = document.querySelector('input')
+    const newEntry = document.createElement('div')
+    newEntry.innerText = input.value
+    entryDiv.append(newEntry)
+}
+
+const provideImputForm = () => {
+    const newForm = document.createElement('form')
+    newForm.innerHTML = `<input id='title placeholder='My Day..' /><br/><input id='entry' placeholder='Dear Diary...' />`
+    entryDiv.append(newForm)
+    button.innerText = 'Post'
+}
 
 const changeFavicon = link => {
     let favicon = document.querySelector('link[rel="icon"]')
@@ -18,7 +35,7 @@ const changeFavicon = link => {
     }
   }
 
+document.addEventListener('DOMContentLoaded', function(){
+    button.addEventListener('click', (e) => getInputOrSubmitNewPost(e))
     changeFavicon("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/anatomical-heart_1fac0.png")
-
-    
 })
