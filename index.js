6e2fd7a33966067b51ec89c1468f9a0f598c7baa
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-02-05 09:59:01 -0500

    idkWhatThisis

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Greg</title>
-    <div>
+document.addEventListener('DOMContentLoaded', function(){
+    const container = document.querySelector('.main-container')
+    const blogInput = document.querySelector('#blogs')
+    const demoInput = document.querySelector('#video')
+    const edInput = document.querySelector('#ed')
+    const workInput = document.querySelector('#work')
+    const buttons = document.querySelector('#buttons')
 
+const changeFavicon = link => {
+    let favicon = document.querySelector('link[rel="icon"]')
+    if (favicon !== null) {
+      favicon.href = link
+    } else {
+      favicon = document.createElement("link")
+      favicon.rel = "icon"
+      favicon.href = link
+      document.head.appendChild(favicon)
+    }
+  }
 
-    </div>
-    <script type="text/javascript" src="index.js" defer></script>
-</head>
-</html>
+    changeFavicon("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/anatomical-heart_1fac0.png")
+
+    
+})
